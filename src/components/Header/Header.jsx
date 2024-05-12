579099a260a884d974b3e80cf5efe011911fe03c
Mukund <mukunddubey10000@gmail.com>
2024-05-12 10:49:27 +0530

    Header Modification for mobile

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import SvgLogo from '../../Assets/HeaderLogo.js'
 import './Header.css'
 import { motion } from 'framer-motion';
+import bars from '../../Assets/bars.png'
 
 const Header = () => {
+  const mobile = window.innerWidth <= 768 ? true : false;
   const transition = { type: 'spring', duration: 0.5 }
+  const [menu, setMenu] = useState(false);
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (menu && !event.target.closest('.header-menu') && !event.target.closest('.dropdown')) {
+        setMenu(false);
+      }
+    };
+    // Add event listener on component mount and remove on unmount
+    document.addEventListener('click', handleClickOutside);
+    return () => {
+      document.removeEventListener('click', handleClickOutside);
+    };
+  }, [menu]);
 
   return (
     <div className='HeaderContainer'>
@@ -17,12 +33,30 @@ const Header = () => {
           <SvgLogo />
         </motion.div>
       </div>
-      <ul className='header-menu'>
-        <li>Services</li>
-        <li>Happy Customers</li>
-        <li>About Us</li>
-        <li>Contact Us</li>
-      </ul>
+      {(!menu && mobile) ?
+        (
+          <div
+            className='dropdown'
+            style={{
+              backgroundColor: 'grey',
+              padding: '0.5rem',
+              borderRadius: '5px',
+              position: 'relative',
+              maxHeight: '1.5rem',
+              marginRight: '10px',
+              marginTop: '10px'
+            }} onClick={() => setMenu(true)}
+          >
+            <img src={bars} alt="" style={{ width: '1.5rem', height: '1.5rem' }} />
+          </div>
+        )
+        : (<ul className='header-menu'>
+          <li onClick={() => setMenu(false)}>Services</li>
+          <li onClick={() => setMenu(false)}>Happy Customers</li>
+          <li onClick={() => setMenu(false)}>About Us</li>
+          <li onClick={() => setMenu(false)}>Contact Us</li>
+        </ul>)
+      }
     </div>
   )
 }
